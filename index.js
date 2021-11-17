a880faf2ff98709caecfc7c19b8c8eacdf91ed30
lvillegas21 <lautaro.villegas.com@gmail.com>
2021-11-17 20:48:24 -0300

    cambios en rutas

--- index.js
+++ index.js
@@ -8,44 +8,14 @@ app.get("/",(req,res)=>{
     res.send("primer servidor con express")
 })
 
-app.get("/productos",(req,res)=>{
-    res.json(contenedor.getAll())
+app.get("/productos",async (req,res)=>{
+    res.json(await contenedor.getAll())
 })
 
-app.get("/productoRandom",(req,res)=>{
-    res.json(contenedor.getById(2))
+app.get("/productoRandom",async (req,res)=>{
+    res.json(await contenedor.getById(1))
 })
 
 app.listen(PORT,()=>{
     console.log(`Mi servidor escuchando desde http://localhost:${PORT}`)
 })
-
-//const newProduct=async()=>{
-  //  let newProduct={
-    //    "title":'gorra',
-    //    "price":2000,
-    //    "thumbnail":'url'
-   // }
-  //  console.log( "id del nuevo producto:",await contenedor.save(newProduct))
-//}
-//newProduct()
-
-//const productoId=async()=>{
-  //  console.log("producto obtenido por id", await contenedor.getById(1))
-//}
-//productoId()
-
-//const allProducts=async()=>{
-  //  console.log("todos los productos", await contenedor.getAll())
-//}
-//allProducts()
-
-//const elimId=async()=>{
-  //  console.log("producto eliminado:", await contenedor.deleteById(2))
-//}
-//elimId()
-
-//const borrarProductos=async()=>{
-  //  console.log("se borraron todos los productos", await contenedor.deleteAll())
-//}
-//borrarProductos()
